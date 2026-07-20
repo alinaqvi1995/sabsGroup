75a605df22ff58399dcccd03aeadb2a7a461d7b9
Ali Naqvi <alinaqvif@gmail.com>
2026-07-20 18:41:17 +0500

    s

--- resources/views/partials/testimonials.blade.php
+++ resources/views/partials/testimonials.blade.php
@@ -2,7 +2,7 @@
 <div class="testimonial_area section-padding">
     <div class="container">
         <div class="section-title text-center">
-            <h2>From Our client</h2>
+            <h2>What Our Clients Say</h2>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
         </div>				
         <div class="row">					
@@ -13,44 +13,47 @@
                             <div class="testimonial_img">
                                 <img src="{{ asset('img/testimonial/1.jpg') }}" alt="testimonial-image" />
                             </div>
-                            <p>Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.</p>
+                            <p>"Working with this team was an absolute pleasure. They delivered a stunning, high-performance website that exceeded all our expectations. Their attention to detail and fast support is unmatched!"</p>
                             <h4>Alex Chohan</h4>
-                            <h5>Director, Accurate themes</h5>
+                            <h5>Director, Accurate Themes</h5>
                         </div>
                     </div><!-- END COL  -->		
+                    
                     <div class="col-lg-6 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
                         <div class="single_testimonial">
                             <div class="testimonial_img">
                                 <img src="{{ asset('img/testimonial/2.jpg') }}" alt="testimonial-image" />
                             </div>
-                            <p>Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.</p>
+                            <p>"The best theme developers we've worked with. The design is modern, clean, and highly customizable. Our sales increased significantly after launching the new website."</p>
                             <h4>Johnson Brown</h4>
-                            <h5>Marketing Head, Spyro themes</h5>
+                            <h5>Marketing Head, Spyro Themes</h5>
                         </div>
                     </div><!-- END COL  -->		
+                    
                     <div class="col-lg-6 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s" data-wow-offset="0">
                         <div class="single_testimonial">
                             <div class="testimonial_img">
                                 <img src="{{ asset('img/testimonial/3.jpg') }}" alt="testimonial-image" />
                             </div>
-                            <p>Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.</p>
-                            <h4>Devid Miller</h4>
-                            <h5>Founder, theme ocean</h5>
+                            <p>"Exceptional quality and professional service. They helped us build a beautiful e-commerce site that our customers love. Highly recommended!"</p>
+                            <h4>David Miller</h4>
+                            <h5>Founder, Theme Ocean</h5>
                         </div>
                     </div><!-- END COL  -->		
+                    
                     <div class="col-lg-6 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.4s" data-wow-offset="0">
                         <div class="single_testimonial">
                             <div class="testimonial_img">
                                 <img src="{{ asset('img/testimonial/4.jpg') }}" alt="testimonial-image" />
                             </div>
-                            <p>Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.</p>
+                            <p>"Outstanding work! The template is not only visually appealing but also very user-friendly and fast. Their team made the entire process smooth and stress-free."</p>
                             <h4>Maya Khan</h4>
-                            <h5>Chairman, Web template</h5>
+                            <h5>Chairman, Web Template</h5>
                         </div>
                     </div><!-- END COL  -->								
                 </div>
             </div>
         </div><!-- END ROW -->				
-    </div><!--- END CONTAINER -->
+    </div><!-- END CONTAINER -->
 </div>
 <!-- END TESTIMONIALS -->
